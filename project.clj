e7147dadb98c041c29759c250768c987f587557f
daslu <daniel.slutsky@gmail.com>
2020-02-23 03:07:45 +0100

    updated notespace ver

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [scicloj/clojisr "1.0.0-BETA7"]
-                 [scicloj/notespace "2.0.0-alpha1"]])
+                 [scicloj/notespace "2.0.0-alpha2"]])
