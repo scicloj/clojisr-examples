3c76ebca40ee1108147a9ce09f75db3fd72c323a
GenerateMe <generateme.blog@gmail.com>
2020-04-12 19:33:27 +0200

    lattice recreated

--- src/clojisr_examples/lattice.clj
+++ src/clojisr_examples/lattice.clj
@@ -24,7 +24,7 @@
 (note-md "```r
 install.packages(c('latticeExtra', 'copula', 'ellipse', 'gridBase',
                    'locfit', 'logspline', 'mapproj', 'maps', 'MEMSS',
-                   'mlmRev', 'RColorBrewer'))
+                   'mlmRev', 'RColorBrewer', 'grid'))
 source('http://bioconductor.org/biocLite.R')
 biocLite(c('flowCore', 'flowViz', 'hexbin'))
 ```")
